cca2e3857a71d4c16a1240d4e99e2c143b0efc5e
Pablo Rossoni <pablorossoni107@outlook.com>
2023-08-23 15:56:35 -0300

    feat: initial products fetch

--- page-capputeeno/src/app/page.tsx
+++ page-capputeeno/src/app/page.tsx
@@ -1,13 +1,17 @@
-
-import { FilterByType } from './components/filter-by-type'
+"use client"
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { FilterBar } from './components/filter-bar'
+import { ProductsList } from './components/products-list'
 import styles from './page.module.css'
 
 export default function Home() {
+  const client = new QueryClient()
   return (
-    <main className={styles.main}>
-      <FilterByType/>
-       
-      
-    </main>
+    <QueryClientProvider client={client}>
+        <main className={styles.main}>
+          <FilterBar/>
+          <ProductsList/>
+        </main>
+    </QueryClientProvider>
   )
 }
