c15f1e57674def6eb6b753b31b434b1d21276928
Pablo Rossoni <pablorossoni107@outlook.com>
2023-08-23 15:55:19 -0300

    feat: created filter bar

--- page-capputeeno/src/app/page.tsx
+++ page-capputeeno/src/app/page.tsx
@@ -1,11 +1,13 @@
+
+import { FilterByType } from './components/filter-by-type'
 import styles from './page.module.css'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
+      <FilterByType/>
        
-      </div>
+      
     </main>
   )
 }
